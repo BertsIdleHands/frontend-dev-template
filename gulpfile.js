4f9b903348b50ff8afb4e883f40e8e1d86dcd480
Bert Vermeire <bert@idlehands.be>
2016-05-24 10:21:10 +0200

    updated version, no more compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,50 @@
-//load plugins
-var gulp             = require('gulp'),
-	compass          = require('gulp-compass'),
-	autoprefixer     = require('gulp-autoprefixer'),
-	minifycss        = require('gulp-minify-css'),
-	uglify           = require('gulp-uglify'),
-	rename           = require('gulp-rename'),
-	concat           = require('gulp-concat'),
-	notify           = require('gulp-notify'),
-	plumber          = require('gulp-plumber'),
-	path             = require('path'),
-	sourcemaps 	     = require('gulp-sourcemaps'),
-	source 			 = require('vinyl-source-stream'),
-	buffer 			 = require('vinyl-buffer'),
-	browserify 		 = require('browserify'),
-	watchify 		 = require('watchify'),
-	babel 			 = require('babelify');
+const gulp = require('gulp');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const sourcemaps = require('gulp-sourcemaps');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const autoprefixer = require('gulp-autoprefixer');
+const minifycss = require('gulp-minify-css');
+const sass = require('gulp-sass');
+const watchify = require('watchify');
+const browserSync = require('browser-sync').create();
 
-var browserSync = require('browser-sync').create();
-
-//styles
-gulp.task('styles', function() {
-	return gulp.src(['./frontend-src/sass/**/*.scss'])
-		.pipe(plumber())
-		.pipe(compass({
-			css: 'public/css',
-			sass: 'frontend-src/sass',
-			image: 'public/img'
-		}))
-		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(gulp.dest('public/css'))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(minifycss())
-		.pipe(gulp.dest('public/css'))
-		.pipe(browserSync.stream());
+gulp.task('styles', function () {
+  return gulp.src(['./frontend-src/sass/**/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./public/css'));
 });
 
-//javascript
-function compile(watch) {
-	var bundler = watchify(browserify('./frontend-src/js/main.js', { debug: true }).transform(babel));
-	bundler.bundle()
-      .on('error', function(err) { console.error(err); this.emit('end'); })
+gulp.task('javascript', function () {
+  return watchify(browserify('./frontend-src/js/main.js', { debug: true }))
+      .transform(babelify, { presets: ['es2015'] })
+      .bundle()
+      .on('error', function (err) { console.error(err); this.emit('end'); })
       .pipe(source('build.js'))
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('./public/js'))
       .pipe(browserSync.stream());
-}
+});
 
-gulp.task('javascript', function() { return compile(); });
-gulp.task('default', ['styles','javascript']);
+gulp.task('default', ['styles', 'javascript']);
 
-gulp.task('watch', function() {
-	browserSync.init({
-        server: {
-            baseDir: "./public"
-        }
-  	});
+gulp.task('watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: './public'
+    }
+  });
 
-  	gulp.watch('./frontend-src/js/**/*.js', ['javascript']);
-  	gulp.watch('./frontend-src/sass/**/*.scss', ['styles']);
-  	gulp.watch('**/*.html').on('change', browserSync.reload);
-  	gulp.watch(['**/*.css']).on("change", function(file) {
-        browserSync.reload(file.path);
-    });
+  gulp.watch('./frontend-src/js/**/*.js', ['javascript']);
+  gulp.watch('./frontend-src/sass/**/*.scss', ['styles']);
+  gulp.watch('**/*.html').on('change', browserSync.reload);
+  gulp.watch(['**/*.css']).on('change', function (file) {
+    browserSync.reload(file.path);
+  });
 });
